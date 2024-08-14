778d711a6c83cc7d965389dccbfa671e6e19ec76
Joenathan <hazendas10@gmail.com>
2024-08-14 16:28:18 +0700

    feat: add spending divider slider

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,28 @@
 import { Text, View } from "react-native";
-import { Button, Card } from "@rneui/themed";
+import { Button, Card, Slider } from "@rneui/themed";
 import { router, useFocusEffect } from "expo-router";
 import { useCallback, useEffect, useState } from "react";
 import { FormatCurrency } from "../../lib/FormatCurrency";
-import { GetMonthlyIncome } from "../../lib/AccessStorage";
+import { GetMonthlyIncome, GetSpendingDivider, StoreSpendingDivider } from "../../lib/AccessStorage";
 
 export default function Home() {
-  const [monthlyIncome, setmonthlyIncome] = useState<string>("0");
+  const [dividerValue, setdividerValue] = useState<number>(0);
+  const [formattedMonthlyIncome, setFormattedMonthlyIncome] =
+    useState<string>("0");
+  const [monthlyIncome, setmonthlyIncome] = useState<number>(0);
+
   useFocusEffect(
     useCallback(() => {
       const GetIncome = async () => {
         const income = await GetMonthlyIncome();
-
+        const DividerPercentage = await GetSpendingDivider();
+        const NumberDividerPercentage = DividerPercentage? +DividerPercentage : 0;
+        setdividerValue(NumberDividerPercentage);
+        const NumberdIncome = income ? +income : 0;
+        setmonthlyIncome(NumberdIncome);
         if (income !== undefined && income !== 0) {
           const formattedIncome = await FormatCurrency(income);
-          setmonthlyIncome(formattedIncome);
-        } else {
-          console.error("Income is either undefined or 0");
+          setFormattedMonthlyIncome(formattedIncome);
         }
       };
 
@@ -25,11 +31,13 @@ export default function Home() {
   );
 
   return (
-    <View>
+    <View style={{ marginVertical: 20 }}>
       <Card>
         <Card.Title>Your Monthly Income</Card.Title>
         <Card.Divider />
-        <Text style={{ textAlign: "center", fontSize: 20, marginVertical: 7 }}>{monthlyIncome}</Text>
+        <Text style={{ textAlign: "center", fontSize: 20, marginVertical: 5 }}>
+          {formattedMonthlyIncome}
+        </Text>
         <Button
           size="sm"
           type="solid"
@@ -39,6 +47,52 @@ export default function Home() {
         >
           Edit
         </Button>
+      </Card>
+      <Card>
+        <Card.Title>Monthly Divider</Card.Title>
+        <Card.Divider />
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 20,
+            marginVertical: 7,
+          }}
+        >
+          Saving | Spending
+        </Text>
+        <Text style={{ textAlign: "center", fontSize: 20, marginVertical: 7 }}>
+          {dividerValue}% | {100 - dividerValue}%
+        </Text>
+        <Slider
+          minimumValue={0}
+          step={1}
+          onSlidingComplete={StoreSpendingDivider}
+          value={dividerValue}
+          onValueChange={setdividerValue}
+          maximumValue={100}
+          thumbStyle={{ height: 20, width: 20, backgroundColor: "#92c5eb" }}
+        ></Slider>
+      </Card>
+      <Card>
+        <Card.Title>Spending Budget</Card.Title>
+        <Card.Divider />
+        <Text style={{ textAlign: "center", fontSize: 20, marginVertical: 7 }}>
+          Rp{" "}
+          {((monthlyIncome * (100 - dividerValue)) / 100).toLocaleString(
+            "id-ID",
+            { minimumFractionDigits: 0 }
+          )}
+        </Text>
+      </Card>
+      <Card>
+        <Card.Title>Saving Budget</Card.Title>
+        <Card.Divider />
+        <Text style={{ textAlign: "center", fontSize: 20, marginVertical: 7 }}>
+          Rp{" "}
+          {((monthlyIncome * dividerValue) / 100).toLocaleString("id-ID", {
+            minimumFractionDigits: 0,
+          })}
+        </Text>
       </Card>
     </View>
   );
