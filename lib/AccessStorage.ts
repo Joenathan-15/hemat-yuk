4166c32a2f857f124ec80e5419b81492f6376bbf
Joenathan <hazendas10@gmail.com>
2024-08-14 16:27:30 +0700

    feat: Spending divider store & get item

--- lib/AccessStorage.ts
+++ lib/AccessStorage.ts
@@ -21,3 +21,25 @@ export async function GetMonthlyIncome(){
         console.error(e)
     }
 }
+
+export async function StoreSpendingDivider(percentage : Number) {
+    const StringPercentage = percentage.toString();
+    try{
+        await AsyncStorage.setItem("DividerPercentage",StringPercentage)
+    }catch(e){
+        console.error(e);
+    }
+}
+
+export async function GetSpendingDivider() {
+    try{
+        const percentage = await AsyncStorage.getItem("DividerPercentage");
+        if(percentage !== null){
+            return percentage
+        }else{
+            return 0
+        }
+    }catch(e){
+        console.error(e);
+    }
+}
